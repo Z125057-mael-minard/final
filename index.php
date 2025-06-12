472655d6401f4b66ff026122fe82e1b786697fa1
Z125056-Luke-Thie <z125056@shibaura-it.ac.jp>
2025-06-12 09:33:43 +0200

    test commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-Commerce</title>
+
+    <?php
+        require "rows/head.php";
+    ?>
+
+</head>
+<body>
+<?php
+    require "rows/header.php";
+
+    require "rows/row-index.php";
+
+    require "rows/footer.php";
+?>
+</body>
+</html>
