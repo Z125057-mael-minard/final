07b0a4b03c0053ff48063ebe383ae34913790fd1
Z125056-Luke-Thie <z125056@shibaura-it.ac.jp>
2025-07-03 09:35:29 +0200

    Header flex style

--- admin/rows/header.php
+++ admin/rows/header.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="container-fluid">
-        <div class="admin_header-container">
+        <div class="admin_header-container d-flex justify-content-evenly">
             <a href="dashboard.php">Dashboard</a>
             <a href="add_product.php">Add product</a>
             <a href="orders.php">Orders</a>
