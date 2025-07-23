288e5232837ac9c2bfd1f1d443e5487f1cb236ca
Z125057-mael-minard <z125057@shibaura-it.ac.jp>
2025-07-23 18:00:28 +0900

    fix address

--- php_scripts/account-change-details.php
+++ php_scripts/account-change-details.php
@@ -23,13 +23,21 @@ $current_session = $stmt->fetch();
 
 $stmt = $db->prepare("UPDATE `users` SET `user_name` = ? WHERE `users`.`user_id` = ?");
 $stmt->execute(array($name, $current_session['user_id']));
-$stmt = $db->prepare("UPDATE `user_addresses` SET `address_country` = ? WHERE `user_addresses`.`user_id` = ?");
-$stmt->execute(array($country, $current_session['user_id']));
-$stmt = $db->prepare("UPDATE `user_addresses` SET `address_city` = ? WHERE `user_addresses`.`user_id` = ?");
-$stmt->execute(array($city, $current_session['user_id']));
-$stmt = $db->prepare("UPDATE `user_addresses` SET `address_street` = ? WHERE `user_addresses`.`user_id` = ?");
-$stmt->execute(array($street, $current_session['user_id']));
-$stmt = $db->prepare("UPDATE `user_addresses` SET `address_house_number` = ? WHERE `user_addresses`.`user_id` = ?");
-$stmt->execute(array($house_nr, $current_session['user_id']));
+$stmt = $db->prepare("SELECT * FROM user_addresses WHERE user_id = ?");
+$stmt->execute(array($current_session['user_id']));
+$addr = $stmt->fetch();
+if ($user == null){
+  $stmt = $db->prepare("INSERT INTO user_addresses (user_id, address_country, address_city, address_street, address_house_number) VALUES (?, ?, ?, ?, ?)");
+  $stmt->execute(array($current_session['user_id'], $country, $city, $street, $house_nr));
+} else {
+  $stmt = $db->prepare("UPDATE `user_addresses` SET `address_country` = ? WHERE `user_addresses`.`user_id` = ?");
+  $stmt->execute(array($country, $current_session['user_id']));
+  $stmt = $db->prepare("UPDATE `user_addresses` SET `address_city` = ? WHERE `user_addresses`.`user_id` = ?");
+  $stmt->execute(array($city, $current_session['user_id']));
+  $stmt = $db->prepare("UPDATE `user_addresses` SET `address_street` = ? WHERE `user_addresses`.`user_id` = ?");
+  $stmt->execute(array($street, $current_session['user_id']));
+  $stmt = $db->prepare("UPDATE `user_addresses` SET `address_house_number` = ? WHERE `user_addresses`.`user_id` = ?");
+  $stmt->execute(array($house_nr, $current_session['user_id']));
+}
 $_SESSION['account_change'] = 'Successfully changed account details';
 header($header);
